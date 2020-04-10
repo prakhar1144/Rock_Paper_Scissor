e5c0b049f65dc0ff47aef1434abd04a78b1b2671
Prakhar Pratyush <56781761+prakhar1144@users.noreply.github.com>
2020-04-11 00:38:16 +0530

    Updated1
    Lot of bug were fixed:
    -> sleep time reduced
    -> cmptr  shifted inside the loop
    -> R P S press option was removed as R can never be equal to rock; which we were comparing in previous code

--- RPS.py
+++ RPS.py
@@ -3,43 +3,43 @@ import time
 
 l = ["rock", "paper", "scissor"]
 
-cmptr = l[randint(0,2)]
-
 Usrnm = input("Please Enter Your Name! ")
 
 print("Hey!", Usrnm, "Welcome")
-time.sleep(2)
-print("The Game is between You and Bot\nThere will be 11 turns\nOne thing to mention is that for your ease press:")
-time.sleep(8)
-print("R for Rock\nP for Paper\nS for Scissor")
+time.sleep(1)
+print("The Game is between You and Bot\nThere will be 7 turns") # \nOne thing to mention is that for your ease press:
+time.sleep(1)
+#print("R for Rock\nP for Paper\nS for Scissor")
 
 
 usrpnt = 0
 cmptrpnt = 0
-for n in range(1,11):
-  User = input("Enter Your Choice!")
+for n in range(1,8):
+  cmptr = l[randint(0, 2)]
+  #print(cmptr)
+  User = str(input("Enter Your Choice!"))
   if User == cmptr:
     print("Oops! Tied")
 
-  elif User == "R":
-      if cmptr == "P":
+  elif User == "rock":
+      if cmptr == "paper":
          cmptrpnt +=1
       else:
           usrpnt +=1
-  elif User == "P":
-      if cmptr == "R":
+  elif User == "paper":
+      if cmptr == "rock":
           usrpnt +=1
       else:
           cmptrpnt +=1
-  elif User == "S":
-      if cmptr == "P":
+  elif User == "scissor":
+      if cmptr == "paper":
           usrpnt +=1
       else:
           cmptrpnt +=1
   else:
       print("Please Enter a valid Choice...!")
 
-  print(11 - n, "turns left")
+  print(7 - n, "turns left")
   print("Score:")
   print(Usrnm, ":", usrpnt)
   print("Bot :", cmptrpnt)
@@ -50,6 +50,7 @@ elif usrpnt == cmptrpnt:
     print("That was close...\nMatch tied...!")
 else:
     print("Better luck next time...!")
+
 
 
 
